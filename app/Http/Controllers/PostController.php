c3345e05836e5278765daf748ca241f8abb38a57
sajjad <mynameissajjad@gmail.com>
2023-02-28 23:51:31 +0330

    add relationship between user and post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function list() {
-       $posts = Post::all();
-
-       return view('home' , compact('posts'));
+       $posts = Post::latest()->with('user')->get();
+       
+       return $posts;
+       //return view('home' , compact('posts'));
     }
 
     public function createPost() {
@@ -21,12 +23,15 @@ class PostController extends Controller
 
     public function storePost(Request $request) {
 
+      
 
-      error_log('create post successfully');
-      $content = $request->input('content');
-      error_log($content);
+      //error_log('create post successfully');
+      //$content = $request->input('content');
+      $content = $request->all();
+      //error_log($content);
 
-      return redirect('/');
+      return $content;
+
     }
 
 }
