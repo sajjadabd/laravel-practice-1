3d8c07399e8bdece84dc9b09de1b47cfbb21475d
sajjad <mynameissajjad@gmail.com>
2023-02-28 18:05:33 +0330

    add about route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/about', function () {
+    return view('about');
+});
+
+
 Route::get('/', function () {
     return view('welcome');
 });
